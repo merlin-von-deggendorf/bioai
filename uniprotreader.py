1865dffa741e709e3761cc2e51c61e63069f691f
merlin <craig4android@gmail.com>
2024-08-04 21:21:30 +0200

    refactor: Improve UniProtKB record parsing and remove unnecessary code

--- uniprotreader.py
+++ uniprotreader.py
@@ -1,4 +1,6 @@
 from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio import SwissProt
 
 # Define the base folder and UniProtKB file name
 base_folder = '/mnt/data/'
@@ -8,15 +10,33 @@ uni_prot_db = 'uniprot_sprot.dat'
 def get_uni_prot_file():
     return base_folder + uni_prot_db
 
-# Get the full file path
-file_path = get_uni_prot_file()
-
-ids = []
-# Parse the UniProtKB flat file
-with open(file_path, 'r') as file:
+def read_annotations():
+    # Get the full file path
+    file_path = get_uni_prot_file()
+    # Parse the UniProtKB flat file using SwissProt
     cntr=0
-    for record in SeqIO.parse(file, 'swiss'):
-        ids.append(record.id)
+    with open(file_path, 'r') as file:
+        for record in SwissProt.parse(file):
+            crossrefs: list[str] = record.cross_references
+            for crossref in crossrefs:
+                print(crossref)
+            cntr+=1
+            if cntr>1000:
+                break   
+        
+            
+def extract_sample(end_line):
+    file_path = get_uni_prot_file()
+    output_file = base_folder + 'sample.dat'
+    # write all the lines until end_line and write them to output_file
+    with open(file_path, 'r') as file:
+        with open(output_file, 'w') as out:
+            for i in range(end_line):
+                line = file.readline()
+                out.write(line)
+                
+            
 
-print('Total number of records:', len(ids))
+read_annotations()
+
 
